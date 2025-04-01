7a2ce383ec5e0c3e3d69ba5e5f4981bb45d48b7d
unknown <20106050@hit.edu.in>
2025-04-01 12:35:09 +0530

    Changed Code

--- test1.py
+++ test1.py
@@ -4,8 +4,8 @@ import sys
 
 print(" Jenkins Build Using Parameters ! ")
 
-X_VALUE = int(sys.argv[0])
+X_VALUE = int(sys.argv[1])
 
-Y_VALUE = int(sys.argv[1])
+Y_VALUE = int(sys.argv[2])
 
 print(" Multiply = ", (X_VALUE * Y_VALUE))
